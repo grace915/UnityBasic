1cf9ff1cc3d28422a1cce7ca03d74e30ccd41db4
grace915 <55349655+grace915@users.noreply.github.com>
2021-02-27 18:40:19 +0900

    week3 2d animation(run, jump)

---
+++ Unity 2D Basic/Assets/Script/PlayerMove.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+    public float jumpPower;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+    int jumpCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        animator.SetBool("isJumping", false);
+        jumpCount = 0;
+    }
+
+    void Update()
+    {
+        /*
+        //jump 1번만 가능
+        if (Input.GetButtonDown("Jump") && !animator.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            animator.SetBool("isJumping", true);
+        }
+        */
+        //jump 2번할래
+        if (Input.GetButtonDown("Jump") && jumpCount < 2)
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            animator.SetBool("isJumping", true);
+            jumpCount++;
+        }
+        
+
+        //stop speed
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.1f, rigid.velocity.y);
+        }
+
+        //direction sprite
+        if (Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+            
+        //animation
+        if (Mathf.Abs(rigid.velocity.x) <0.1f   )
+            animator.SetBool("isWalking", false);
+        else
+            animator.SetBool("isWalking", true);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //move speed
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        //max speed
+        if (rigid.velocity.x > maxSpeed)//right max speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        if (rigid.velocity.x < maxSpeed*(-1))//left max speed
+            rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
+
+        if(rigid.velocity.y < 0)
+        {
+            //Landing platform
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(1, 0, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                {
+                    animator.SetBool("isJumping", false);
+                    jumpCount = 0;
+
+                }
+
+            }
+        }
+
+    }
+}
