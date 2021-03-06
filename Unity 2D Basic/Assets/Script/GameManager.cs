a83a98140ce123189441ea2922a10c98e20e36e0
grace915 <55349655+grace915@users.noreply.github.com>
2021-03-06 22:41:03 +0900

    week4 2d 게임 만들기 완성

---
+++ Unity 2D Basic/Assets/Script/GameManager.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int totalPoint;
+    public int stagePoint;
+    public int stageIndex;
+    public int health;
+    public GameObject[] Stages;
+
+    public PlayerMove player;
+
+    //ui
+    public Image[] UIhealth;
+    public Text UIPoint;
+    public Text UIStage;
+    public GameObject UIRestartBtn;
+
+    private void Update()
+    {
+        //point
+        UIPoint.text = ( totalPoint + stagePoint).ToString();
+    }
+    // Start is called before the first frame update
+    public void NextStage()
+
+    {   // change stage
+        if (stageIndex < Stages.Length-1)
+        {            
+            Stages[stageIndex].SetActive(false);
+            stageIndex++;
+            Stages[stageIndex].SetActive(true);
+            PlayerReposition();
+
+            //calculate point
+            totalPoint += stagePoint;
+            stagePoint = 0;
+
+
+            UIStage.text = "STAGE " + (stageIndex + 1);
+        }
+        else
+        {
+            //game clear
+            //player control lock
+            Time.timeScale = 0;
+            //result ui
+            Debug.Log("게임 클리어");
+
+            //restart button ui
+            Text btnText = UIRestartBtn.GetComponentInChildren<Text>();
+            btnText.text = "Clear!";
+            UIRestartBtn.SetActive(true);
+            
+        }
+        
+
+
+    }
+
+    public void HealthDown()
+    {
+        if (health > 1)
+        {
+            health--;
+            UIhealth[health].color = new Color(1, 0, 0, 0.4f);
+            
+        }
+            
+        
+        else
+        {
+            //all health ui off
+            UIhealth[0].color = new Color(1, 0, 0, 0.4f);
+            //player die effect
+            player.OnDie();
+            //result UI
+            Debug.Log("Die");
+            //retry button ui
+            UIRestartBtn.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+           
+
+            //plyer reposition
+            if (health > 1)
+            {
+                PlayerReposition();
+            }
+
+            //health down
+            HealthDown();
+
+
+        }
+    }
+
+    void PlayerReposition()
+    {
+        player.transform.position = new Vector3(0, 0, -1);
+        player.VelocityZero();
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+
+    }
+
+   
+
+}
--- Unity 2D Basic/Assets/Script/PlayerMove.cs
+++ Unity 2D Basic/Assets/Script/PlayerMove.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+
+    public GameManager gameManager;
+    public AudioClip audioJump;
+    public AudioClip audioAttack;
+    public AudioClip audioDamaged;
+    public AudioClip audioItem;
+    public AudioClip audioDie;
+    public AudioClip audioFinish;
     public float maxSpeed;
     public float jumpPower;
+
+
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator animator;
     int jumpCount;
+    CapsuleCollider2D capsuleCollider;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +29,39 @@ public class PlayerMove : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
         animator.SetBool("isJumping", false);
         jumpCount = 0;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void PlaySound(string action)
+    {
+        switch (action)
+        {
+            case "JUMP":
+                audioSource.clip = audioJump;
+                break;
+            case "ATTACK":
+                audioSource.clip = audioAttack;
+                break;
+            case "DAMAGED":
+                audioSource.clip = audioDamaged;
+                break;
+            case "ITEM":
+                audioSource.clip = audioItem;
+                break;
+            case "DIE":
+                audioSource.clip = audioDie;
+                break;
+            case "FINISH":
+                audioSource.clip = audioFinish;
+                break;
+
+        }
+
+        audioSource.Play();
+
     }
 
     void Update()
@@ -37,6 +80,7 @@ public class PlayerMove : MonoBehaviour
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             animator.SetBool("isJumping", true);
             jumpCount++;
+            PlaySound("JUMP");
         }
         
 
@@ -99,18 +143,56 @@ public class PlayerMove : MonoBehaviour
             if(rigid.velocity.y < 0 && transform.position.y > collision.transform.position.y)
             {
                 OnAttack(collision.transform);
+                PlaySound("ATTACK");
             }
             else //Damaged
             {
                 OnDamaged(collision.transform.position);
+                PlaySound("DAMAGED");
             }
             
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+         
+        if (collision.gameObject.tag == "Item")
+        {
+           
+            //Point
+            bool isBronze = collision.gameObject.name.Contains("Bronze");
+            bool isSilver = collision.gameObject.name.Contains("Silver");
+            bool isGold = collision.gameObject.name.Contains("Gold");
+
+            if (isBronze)
+                gameManager.stagePoint += 50;
+            if (isSilver)
+                gameManager.stagePoint += 100;
+            if (isGold)
+                gameManager.stagePoint += 300;
+
+            //Deactive Item
+            collision.gameObject.SetActive(false);
+
+            //sound
+            PlaySound("ITEM");
+        }
+        else if(collision.gameObject.tag == "Finish")
+        {
+            //Next stage
+            gameManager.NextStage();
+            // sound
+            PlaySound("FINISH");
+
         }
     }
 
     void OnAttack(Transform enemy)
     {
         //Point
+        gameManager.stagePoint += 100;
 
         //Reaction Force
         rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
@@ -123,6 +205,10 @@ public class PlayerMove : MonoBehaviour
     //무적상태
     void OnDamaged(Vector2 targetPos)
     {
+
+        //Health down
+        gameManager.HealthDown();
+
         //change layer
         gameObject.layer = 11;
 
@@ -145,5 +231,26 @@ public class PlayerMove : MonoBehaviour
     {
         gameObject.layer = 10;
         spriteRenderer.color = new Color(1, 1, 1, 1);
+    }
+
+    public void OnDie()
+    {
+        //spirte alpha
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+        //sprite flip y
+        spriteRenderer.flipY = true;
+        //collider disable
+        capsuleCollider.enabled = false;
+        //die effect jump
+        rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        PlaySound("DIE");
+
+
+
+    }
+
+    public void VelocityZero()
+    {
+        rigid.velocity = Vector2.zero;
     }
 }
